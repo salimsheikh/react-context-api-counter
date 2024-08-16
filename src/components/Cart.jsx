2cc9d9b808a87f67ab81aef7b67dad30b34e9198
salimsheikh <salimsheikh4u2000@gmail.com>
2024-08-16 10:52:36 +0530

    add to cart completed

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CartContext } from '../contexts/CartContext'
 
 const Cart = () => {
+  const cart = useContext(CartContext);
+  let a = 0;
+  const total = cart.items.reduce((a, b) => a + b.price, 0)
   return (
     <div className='cart'>
-
         <ul>
-            <li>Keyboard $900</li>
+          {cart && cart.items.map((item, index) => (<li key={item.name + index}>{index}  {item.name} ${item.price}</li>))}          
         </ul>
 
-        <h5>Total - $9000</h5>
+        <h5>Total - ${total}</h5>
     </div>
   )
 }
